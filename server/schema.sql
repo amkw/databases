3be99f2cd03e0cd48a05c4ce4f81185f2f7dfbdc
TuyetVo <ttv5004@gmail.com>
2019-05-01 23:37:09 -0400

    Update: create database/ tables, imported chatterbox client

--- server/schema.sql
+++ server/schema.sql
@@ -4,11 +4,27 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id int AUTO_INCREMENT PRIMARY KEY,
+  username TEXT,
+  comments TEXT,
+  rooms TEXT,
+  tstamp TIMESTAMP,
+  FOREIGN KEY(rooms) REFERENCES rooms(id),
+  FOREIGN KEY(username) REFERENCES usernames(id)
+
 );
 
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE rooms (
+  id int AUTO_INCREMENT PRIMARY KEY,
+  name TEXT
+);
 
+CREATE TABLE usernames (
+  id int AUTO_INCREMENT PRIMARY KEY,
+  name TEXT
+);
 
 
 /*  Execute this file from the command line by typing:
