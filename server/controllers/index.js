6808678348e8fa0e81e116eff8825bda59828e13
Alyssa Wei <wei.alyssa@gmail.com>
2019-05-02 20:57:24 -0700

    Refactoring for Sequelize ORM, in process

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,42 +1,66 @@
 var models = require('../models');
-// var bluebird = require('bluebird');
-// var http = require('http');
+var bluebird = require('bluebird');
+var db = require('../db');
 
-// var userFields = ['username'];
-// var messageFields = ['messages', 'username', 'roomname'];
+
+var userFields = ['username'];
+var messageFields = ['message', 'username', 'roomname'];
 
 module.exports = {
 
   messages: {
     get: function (req, res) {
-      models.messages.get((err, results) => {
-        if (err) { console.error(err) }
-        res.json(results);
-      });
+      db.Message.findAll({include: [db.User]})
+        .then(function(messages) {
+          res.json(messages);
+        });
+      // models.messages.get((err, results) => {
+      //   if (err) { console.error(err) }
+      //   res.json(results);
+      // });
     }, // a function which handles a get request for all messages
     post: function (req, res) {
-      var params = [ req.body.message, req.body.username, req.body.roomname ];
-      models.messages.post(params, (err, results) => {
-        if (err) { console.error(err) }
-        res.sendStatus(201);
-      });
+      db.User.findOrCreate({where: {username: req.body.username}})
+        .spread(function(user, created) {
+          db.Message.create({
+            userid: user.get('id'),
+            text: req.body.message,
+            roomname: req.body.roomname
+          }).then(function(message) {
+            res.sendStatus(201);
+          });
+        });
+
+      // models.messages.post(params, (err, results) => {
+      //   if (err) { console.error(err) }
+      //   res.sendStatus(201);
+      // });
     } // a function which handles posting a message to the database
   },
 
   users: {
-    // Ditto as above
+
     get: function (req, res) {
-      models.users.get((err, results) => {
-        if (err) { console.error(err) }
-        res.json(results);
-      });
+      db.User.findAll()
+        .then(function(users) {
+          res.json(users);
+        });
+      // models.users.get((err, results) => {
+      //   if (err) { console.error(err) }
+      //   res.json(results);
+      // });
     },
     post: function (req, res) {
-      var params = [ req.body.username ];
-      models.users.post(params, (err, results) => {
-        if (err) { console.error(err) }
-        res.sendStatus(201);
-      });
+      db.User.findOrCreate({where: {username: req.body.username}})
+        .spread(function(user, created) {
+          res.sendStatus(created ? 201 : 200);
+        });
+      // var params = [ req.body.username ];
+      // models.users.post(params, (err, results) => {
+      //   if (err) { console.error(err) }
+      //   res.sendStatus(201);
+      // });
+
     }
   }
 };
--- server/db/index.js
+++ server/db/index.js
@@ -1,16 +1,37 @@
-var mysql = require('mysql');
+var Sequelize = require('sequelize');
+var orm = new Sequelize('chat', 'root', '');
 
-// Create a database connection and export it from this file.
-// You will need to connect with the user "root", no password,
-// and to the database "chat".
-
-var connection = mysql.createConnection({
-  user: 'root', // changed from student
-  password: '', // changed from student
-  database: 'chat'
+var User = orm.define('User', {
+  username: Sequelize.STRING
 });
 
-connection.connect();
+var Message = orm.define('Message', {
+  text: Sequelize.STRING, //could be message
+  roomname: Sequelize.STRING
+});
 
-module.exports = connection;
+User.hasMany(Message);
+Message.belongsTo(User);
 
+User.sync();
+Message.sync();
+
+exports.User = User;
+exports.Message = Message;
+
+
+// var mysql = require('mysql');
+
+// // Create a database connection and export it from this file.
+// // You will need to connect with the user "root", no password,
+// // and to the database "chat".
+
+// var connection = mysql.createConnection({
+//   user: 'root', // changed from student
+//   password: '', // changed from student
+//   database: 'chat'
+// });
+
+// connection.connect();
+
+// module.exports = connection;
